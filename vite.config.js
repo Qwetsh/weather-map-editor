1ee795f144c06a350c077b573ac7950d679167e6
Qwetsh <tomicharles@gmail.com>
2025-12-23 00:17:51 +0100

    Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-map-editor/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
