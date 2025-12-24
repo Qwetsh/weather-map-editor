07e73e4f6286464abfd499cf591e53f1373a5756
Qwetsh <tomicharles@gmail.com>
2025-12-24 15:46:24 +0100

    Fix GH Pages base and rebuild docs

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/weather-map-editor/',
+  base: '/weatherMap/',
   build: {
     outDir: 'docs',
   },
